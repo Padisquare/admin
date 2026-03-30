295a0d5991fed6d87eceb2dc14011ac235222019
CasManny <chukwusabastine2018@gmail.com>
2026-03-30 17:45:30 +0100

    fix merge conflict

--- src/app/(dashboard)/settings/page.tsx
+++ src/app/(dashboard)/settings/page.tsx
@@ -1,14 +1,5 @@
 "use client";
-import CustomButton from "@/components/common/custom-button";
-import CustomPagination from "@/components/common/custom-pagination";
-import { CustomTable } from "@/components/common/custom-table";
-import { adminsTableColumns } from "@/components/setting/admin-column";
-import AdminInvite from "@/components/setting/admin-invite";
-import CreateWebsiteTemplate from "@/components/setting/create-website-template";
 import SettingsHub from "@/components/setting/settings-hub";
-import { Input } from "@/components/ui/input";
-import { adminsData } from "@/constants/data";
-import { Search } from "lucide-react";
 
 const SettingsHomepage = () => {
   return (
